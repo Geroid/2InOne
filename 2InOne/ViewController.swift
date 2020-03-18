a232ec2700b828daf6a3add65ccd1d6b839145e0
Viktor Rezvantsev <geroid1735@gmail.com>
2020-03-18 23:27:11 +0300

    initial commit

---
+++ 2InOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2InOne
+//
+//  Created by Viktor Rezvantsev on 18.03.2020.
+//  Copyright © 2020 Viktor Rezvantsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
