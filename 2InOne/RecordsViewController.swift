ed409456ecec19b6454d7fcc3d75418e8fbba55b
Viktor Rezvantsev <geroid1735@gmail.com>
2020-04-03 12:02:20 +0300

    fix Records table

--- 2InOne/RecordsViewController.swift
+++ 2InOne/RecordsViewController.swift
@@ -16,7 +16,9 @@ enum Games {
 
 class RecordsViewController: UIViewController, UITableViewDelegate {
     
+    // MARK: Properties
     var recordItems: [GameResult]?
+    let cellReuseIdentifier = "cell"
     
     // MARK: Outlets
     @IBOutlet private var tableView: UITableView!
@@ -24,24 +26,27 @@ class RecordsViewController: UIViewController, UITableViewDelegate {
     // MARK: Override funcs
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     // MARK: Public funcs
     static func instantiate2() -> RecordsViewController {
         return UIStoryboard(name: "Records",
                             bundle: nil).instantiateViewController(withIdentifier: "RecordsViewController") as! RecordsViewController
+        
     }
     
     // MARK: IBAction
     @IBAction private func gameDidChange(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
         case 0:
-            view.backgroundColor = .white
             recordItems = Games.game1.records.results()
-            print(recordItems?.first)
         case 1:
             view.backgroundColor = .white
-            print(Games.records.results())
+            recordItems = Games.records.results()
         default:
             view.backgroundColor = .white
         }
@@ -49,24 +54,21 @@ class RecordsViewController: UIViewController, UITableViewDelegate {
     }
 }
 
-// MARK: UITableViewDelegate
+// MARK: UITableViewDataSource
 extension RecordsViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("Test \(Games.game1.records.results().count)")
         return recordItems?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
         
         guard let recs = recordItems else {
             return UITableViewCell()
         }
-        //        cell.setValue(recs, forKey: "result")
         let item = "Number: \(recs[indexPath.row].number) Tries: \(recs[indexPath.row].tries)"
         cell.textLabel?.text =  item
-        print(item)
         return cell
     }
     
